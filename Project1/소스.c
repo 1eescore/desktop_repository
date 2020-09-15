b14364d73301205a4fb301c46d91fc244b28dd0e
skolea4 <50323155+skolea4@users.noreply.github.com>
2020-09-15 16:40:56 +0900

    tset1
    test1

---
+++ Project1/소스.c
@@ -0,0 +1,347 @@
+//#include<stdio.h>
+//int main()
+//{
+//	printf("hello world");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%s", "Fun Programming!");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%s", "Programming! It's fun.");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("My name is Hong Gil Dong.\nI am 13 years old.");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("(@) (@)\n(=^.^=)\n(-m-m-)");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%s\n%s", "I can program well.", "Dreams come true.");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%s", "I can program well.\nDreams come true.");
+//	return 0;
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%s\n%d\n%s\n%.6lf", "My height", 170, "My weight", 68.6);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%s", "5 Dan\n5 * 2 = 10");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%10s%10s%10s\n%10s%10s%10s\n%10s%10s%10s\n%10s%10s%10s", "item", "count", "price", "pen", "20", "100", "note", "5", "95", "eraser", "110", "97");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%s", "My name is Hong");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%s", "My hometown\nFlowering mountain");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%s", "TTTTTTTTTT\nTTTTTTTTTT\n    TT\n    TT\n    TT");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int sum = 0;
+//	double avg = 0;
+//	printf("%s", "kor 90\nmat 80\neng 100");
+//	sum = 90 + 80 + 100;
+//	avg = sum / 3;
+//	printf("\n%s %d\n%s %d", "sum", sum, "avg", (int)avg);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%15s%15s%15s\n", "Seoul", "10,312,545", "+91,375");
+//	printf("%15s%15s%15s\n", "Pusan", "3,567,910", "+5,868");
+//	printf("%15s%15s%15s\n", "Incheon", "2,758,296", "+64,888");
+//	printf("%15s%15s%15s\n", "Daegu", "2,511,676", "+17,230");
+//	printf("%15s%15s%15s", "Gwangju", "1,454,636", "+29,774");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int num = -100;
+//	printf("%d", num);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int num1 = -1, num2 = 100;
+//	printf("%d\n%d", num1, num2);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int num1 = 55, num2 = 10, num3 = 2008, num4 = 1999;
+//	printf("%d - %d = %d\n%d - %d = %d", num1, num2, num1 - num2, num3, num4, num3 - num4);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int  weight = 49;
+//	double force = 0.2683, multiple ;
+//	multiple = weight * force;
+//	printf("%d * %f = %.6lf", weight, force, multiple);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	double yd = 91.44, in = 2.54;
+//	printf("%4.1fyd = %5.1fcm\n", 2.1, yd * 2.1);
+//	printf("%4.1fin = %5.1fcm", 10.5, in * 10.5);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int height;
+//	scanf("height = %d", &height);
+//	printf("%d", height);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("%d * %d = %d\n", a, b, a * b);
+//	printf("%d / %d = %d", a, b, a / b);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	double a, b = 0;
+//	char c ;
+//	scanf("%lf\n%lf\n%c", &a, &b, &c);
+//	printf("%.2lf\n%.2lf\n%c", a, b, c);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	double a, b, c = 0;
+//	scanf("%lf\n%lf\n%lf", &a, &b, &c);
+//	printf("%.3lf\n%.3lf\n%.3lf", a, b, c);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c = 0;
+//	a = 10;
+//	b = 20;
+//	c = 30;
+//	printf("%d + %d = %d", a, b, c);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	double a = 80.5, b = 22.34;
+//	printf("%10.2lf%10.2lf%10.2lf", a, b, a + b);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 50;
+//	double b = 100.12;
+//	printf("%.2lf * %d = %d", b, a, (int)((float)a * b));
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c;
+//	scanf("%d %d %d", &a, &b, &c);
+//	printf("sum = %d\navg = %d", a + b + c, (int)(a + b + c) / 3);
+//	return 0;
+//
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	double a = 0;
+//	printf("yard? ");
+//	scanf("%lf", &a);
+//	printf("%.1lfyard = %.1lfcm", a, a * 91.44);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{	
+//	int a, b, c = 0;
+//	scanf("%d %d %d", &a, &b, &c);
+//	printf("sum : %d\navg : %d", a + b + c, (a + b + c) / 3);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("%d %d", a + 100, b % 10);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	printf("%d\n", a++);
+//	printf("%d", ++a);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b,c= 0;
+//	scanf("%d %d", &a, &b);
+//	c = a++ * --b;
+//	printf("%d %d %d", a, b, c);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b = 0;
+//	scanf("%d %d", &a, &b);
+//	if (a == b)
+//	{
+//		printf("1\n0");
+//	}
+//	else
+//	{
+//		printf("0\n1");
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	printf("%d > %d --- %d\n", a, b, a > b);
+//	printf("%d < %d --- %d\n", a, b, a < b);
+//	printf("%d >= %d --- %d\n", a, b, a >= b);
+//	printf("%d <= %d --- %d", a, b, a <= b);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("%d %d", a * b != 0, a + b != 0);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c = 0;
+//	scanf("%d %d %d", &a, &b, &c);
+//	printf("%d %d", (a > b) && (a > c), (a == b) && (b== c));
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c, d = 0;
+//	scanf("%d %d %d %d", &a, &b, &c, &d);
+//	printf("sum %d\navg %d", a + b + c + d, (a + b + c + d) / 4);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("%d / %d = %d...%d", a, b, a / b, a % b);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b = 0;
+//	scanf("%d %d", &a, &b);
+//	a += 5;
+//	b *= 2;
+//	printf("width = %d\nlength = %d\narea = %d",a,b,a*b);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("%d %d\n", ++a, b--);
+//	printf("%d %d", a, b);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c,d = 0;
+//	scanf("%d %d\n%d %d", &a, &b, &c, &d);
+//	printf("%d", (a > c) && (c > d));
+//	return 0;
+//}
+#include <stdio.h>
+
+int main() {
+	printf("Hello world!");
+
+	int num = 10;
+	printf(num);
+
+	//주석 처리 아 근데 오 ㅐ안되는거야 미치겠눼
+	//오쉣 이렇고 안되면 어쩌누 오쉐에엥스에강ㄴ기ㅏㄴ어ㅣ가넝
+
+	return 0;
+}
